ea301ca6fc19a6892af4f11fac1996ba29a5b55e
Dev-IamNk <nandhakumarannkit@gmail.com>
2025-10-03 17:39:09 +0530

    Initial commit

---
+++ Isomorphic.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Isomorphic {
+    public boolean isIsomorphic(String s, String t) {
+        HashMap<Character, Character> hm = new HashMap<>();
+        HashMap<Character, Character> hm2 = new HashMap<>();
+        if (s.length() != t.length())
+            return false;
+        for (int i = 0; i < s.length(); i++) {
+            char c1 = s.charAt(i);
+            char c2 = t.charAt(i);
+            if (hm.containsKey(c1) && hm.get(c1) != c2)
+                return false;
+            if (hm2.containsKey(c2) && hm2.get(c2) != c1)
+                return false;
+            hm.put(c1, c2);
+            hm2.put(c2, c1);
+        }
+        return true;
+    }
+}
